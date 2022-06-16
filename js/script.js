5805cd006b4c8029754177c887fc154be9964ba0
Mary-Frye <deimospho@gmail.com>
2022-06-15 22:40:35 -0400

    css html js changes for images

--- js/script.js
+++ js/script.js
@@ -3,87 +3,87 @@ let headsFlips = 0
 let tailsFlips = 0
 
 //Listen for DOMContentLoaded to ensure that all HTML and resources have been loaded before attepting to run code
-    //Add event listeners and handlers for both buttons
-    document.getElementById('flip').addEventListener('click', () => {
-        //Calculate Probablities
-        //Coin animation
-        function toss() {
-            const coin = document.getElementById("coin");
-            coin.style.animation = "none";
-            if (Math.random() <= 0.5) {
-                coin.style.animation = "headsFlips 3s forwards";
-            } else {
-                coin.style.animation = "tailsFlips 3s forwards";
-            }
+//Add event listeners and handlers for both buttons
+document.getElementById('flip').addEventListener('click', () => {
+    //Calculate Probablities
+    //Coin animation
+    function toss() {
+        const coin = document.getElementById("coin");
+        coin.style.animation = "none";
+        if (Math.random() <= 0.5) {
+            coin.style.animation = "headsFlips 3s forwards";
+        } else {
+            coin.style.animation = "tailsFlips 3s forwards";
         }
-        //Take different actions dependent on result
-        let flippedHeads= Math.random() < 0.5
-        if (flippedHeads) {
-            //display image and message as heads
-            document.getElementById('coinImage').src = 'assets/images/belizeCoinHeads.png';
-            document.getElementById('message').textContent = "You Flipped Heads!";
+    }
+    //Take different actions dependent on result
+    let flippedHeads = Math.random() < 0.5
+    if (flippedHeads) {
+        //display image and message as heads
+        document.getElementById('coinImage').src = 'https://photos.app.goo.gl/4hPDKGTBXA6EE47b9';
+        document.getElementById('message').textContent = "You Flipped Heads!";
 
-            //Add count to tally
-            headsFlips += 1;
-        }
-        else {
-            //Display image and message as tails
-            document.getElementById('coinImage').src = 'assets/images/belizeCoinTails.png';
-            document.getElementById('message').textContent = "You Flipped Tails!";
+        //Add count to tally
+        headsFlips += 1;
+    }
+    else {
+        //Display image and message as tails
+        document.getElementById('coinImage').src = 'https://photos.app.goo.gl/LGMRSwcFWRWB37Sa9';
+        document.getElementById('message').textContent = "You Flipped Tails!";
 
-            //Add count to tally
-            tailsFlips += 1;
-        }
-        //Update the scoreboard
-        //Calculate total number of rolls
-        let total = headsFlips + tailsFlips;
+        //Add count to tally
+        tailsFlips += 1;
+    }
+    //Update the scoreboard
+    //Calculate total number of rolls
+    let total = headsFlips + tailsFlips;
 
-        //Create variables to track percent heads and tails
-        let percentHeads = 0;
-        let percentTails = 0;
+    //Create variables to track percent heads and tails
+    let percentHeads = 0;
+    let percentTails = 0;
 
-        //Before trying to divide, make sure the total is not zero
-        if (total > 0) {
-            //Calculate percentage of heads and tails
-            percentHeads = Math.round((headsFlips / total) * 100);
-            percentTails = Math.round((tailsFlips / total) * 100);
+    //Before trying to divide, make sure the total is not zero
+    if (total > 0) {
+        //Calculate percentage of heads and tails
+        percentHeads = Math.round((headsFlips / total) * 100);
+        percentTails = Math.round((tailsFlips / total) * 100);
 
-        }
-        //Update the values in the scoreboard table
-        document.getElementById('heads').textContent = headsFlips;
-        document.getElementById('tails').textContent = tailsFlips;
-        document.getElementById('heads-percent').textContent = percentHeads + '%';
-        document.getElementById('tails-percent').textContent = percentTails + '%';
+    }
+    //Update the values in the scoreboard table
+    document.getElementById('heads').textContent = headsFlips;
+    document.getElementById('tails').textContent = tailsFlips;
+    document.getElementById('heads-percent').textContent = percentHeads + '%';
+    document.getElementById('tails-percent').textContent = percentTails + '%';
 
-    })
+})
 
-    document.getElementById('clear').addEventListener('click', function() {
-        //Reset the heads and tails value to zero
-        headsFlips = 0
-        tailsFlips = 0
+document.getElementById('clear').addEventListener('click', function () {
+    //Reset the heads and tails value to zero
+    headsFlips = 0
+    tailsFlips = 0
 
-        //Update the message to the user
-        document.getElementById('message').textContent = "Let's Get Started!"
+    //Update the message to the user
+    document.getElementById('message').textContent = "Let's Get Started!"
 
-        //Update the scoreboard
+    //Update the scoreboard
 
-            //Calculate total number of rolls
-            let total = headsFlips + tailsFlips
+    //Calculate total number of rolls
+    let total = headsFlips + tailsFlips
 
-        //Create variables to track percent heads and tails
-        let percentHeads = 0
-        let percentTails = 0
+    //Create variables to track percent heads and tails
+    let percentHeads = 0
+    let percentTails = 0
 
-        //Befgore trying to divide, make sure the total is not zero
-        if (total > 0) {
-            //Calculate percentage of heads and tails
-            percentHeads = Math.round((headsFlips / total) * 100)
-            percentTails =  Math.round((tailsFlips / total) * 100)
+    //Befgore trying to divide, make sure the total is not zero
+    if (total > 0) {
+        //Calculate percentage of heads and tails
+        percentHeads = Math.round((headsFlips / total) * 100)
+        percentTails = Math.round((tailsFlips / total) * 100)
 
-        }
-        //Update the values in the scoreboard table
-        document.getElementById('heads').textContent = headsFlips
-        document.getElementById('heads-percent').textContent = percentHeads + '%'
-        document.getElementById('tails').textContent = tailsFlips
-        document.getElementById('tails-percent').textContent = percentTails + '%'   
-    })
+    }
+    //Update the values in the scoreboard table
+    document.getElementById('heads').textContent = headsFlips
+    document.getElementById('heads-percent').textContent = percentHeads + '%'
+    document.getElementById('tails').textContent = tailsFlips
+    document.getElementById('tails-percent').textContent = percentTails + '%'
+})
